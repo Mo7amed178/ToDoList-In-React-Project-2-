c2543f7d4c9f129fe9472129875b2962e0b3e23f
Mohammed <moh.jameel178@gmail.com>
2019-02-20 16:05:19 +0300

    Updates

---
+++ precache-manifest.685b859808fa91db9ec430ea106caca1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c9a6a763bd58e299e2d",
+    "url": "/ToDoList-In-React-Project-2-/static/css/main.5abdc549.chunk.css"
+  },
+  {
+    "revision": "2c9a6a763bd58e299e2d",
+    "url": "/ToDoList-In-React-Project-2-/static/js/main.2c9a6a76.chunk.js"
+  },
+  {
+    "revision": "e0c953a260259e84e4a1",
+    "url": "/ToDoList-In-React-Project-2-/static/js/runtime~main.e0c953a2.js"
+  },
+  {
+    "revision": "e6be137ed5794e8e3798",
+    "url": "/ToDoList-In-React-Project-2-/static/js/2.e6be137e.chunk.js"
+  },
+  {
+    "revision": "e63eb622e0465c92819ad2740e15341b",
+    "url": "/ToDoList-In-React-Project-2-/index.html"
+  }
+];
